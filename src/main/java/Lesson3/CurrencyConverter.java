aa86e03169ee5216827e34e8c41b5a9c9d25656d
viacheslav.c <s.chugaevsky@gmail.com>
2023-12-07 20:01:29 +0200

    HomeWork lesson_3

--- src/main/java/Lesson3/CurrencyConverter.java
+++ src/main/java/Lesson3/CurrencyConverter.java
@@ -1,4 +1,36 @@
 package Lesson3;
 
 public class CurrencyConverter {
+    private double commission ;
+    private double exchangeRate;
+
+    public CurrencyConverter(double commission, double exchangeRate) {
+        this.commission = commission;
+        this.exchangeRate = exchangeRate;
+    }
+    private double convertUsdToUah(double amount) {
+    double result;
+    result = amount * exchangeRate;
+
+    return result;
+    }
+
+    private double calculateCommission(double amount) {
+    double result;
+    result = this.convertUsdToUah(amount) * commission ;
+
+    return result;
+    }
+
+
+    public double calculateTotalAmount(double amount){
+    double result;
+    result = this.convertUsdToUah(amount) - this.calculateCommission(amount);
+
+    return result;
+    }
+
+
 }
+
+
--- src/main/java/Lesson3/Main.java
+++ src/main/java/Lesson3/Main.java
@@ -1,7 +1,18 @@
 package Lesson3;
-
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-    System.out.println("test");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input amount for convert USD to UAH:");
+        double amount = sc.nextDouble();
+
+        CurrencyConverter test1 = new CurrencyConverter(0.01,36.55);
+        String result = String.format("%.2f",test1.calculateTotalAmount(amount));
+        System.out.println("Amount in UAH after conversion: " + result);
+        System.out.println("Goodbye, have a good day!!!");
+
+
+
     }
 }
