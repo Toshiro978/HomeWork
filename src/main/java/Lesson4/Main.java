24d79015276749767e264dd40093bfaf457cf792
viacheslav.c <s.chugaevsky@gmail.com>
2023-12-12 20:10:30 +0200

    HomeWork Lesson4

---
+++ src/main/java/Lesson4/Main.java
@@ -0,0 +1,39 @@
+package Lesson4;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int arr[] = {1,7,31,4,-2,4,-1,5,18,99,32};
+        System.out.println("Write 1 in terminal to output Triangle");
+        System.out.println("Write 2 in terminal to output Rhomb");
+        System.out.println("Write 3 in terminal to output sorted massive");
+        Scanner sc = new Scanner(System.in);
+        int KeyInterface = sc.nextInt();
+
+
+        Figure Illustration = new Figure();
+        switch (KeyInterface){
+            case (1):Illustration.OutPutTriangle();
+            break;
+            case (2): Illustration.OutPutRhomb();
+            break;
+            case (3):
+                for (int num: arr)
+                    System.out.print(num + " ");
+
+                Sorting.bubbleSort(arr);
+                System.out.println();
+
+                for (int num: arr
+                ) {
+                    System.out.print(num + " ");
+
+                }
+                break;
+         }
+
+
+    }
+}
---
+++ src/main/java/Lesson4/Sorting.java
@@ -0,0 +1,20 @@
+package Lesson4;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Sorting {
+
+    static void bubbleSort(int @NotNull [] arr) {
+        int temp = 0;
+        int n = arr.length;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
