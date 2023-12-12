3995a2c614b91eedfe80a06c8b1075a685af830c
viacheslav.c <s.chugaevsky@gmail.com>
2023-12-12 20:13:22 +0200

    HomeWork Lesson4

--- src/main/java/Lesson4/Figure.java
+++ src/main/java/Lesson4/Figure.java
@@ -4,12 +4,12 @@ public class Figure {
 
     public void OutPutTriangle() {
         int count = 0;
-        for (int i=0; i<=4; ++i) {
-            for (int j=4; i<=j; --j){
+        for (int i = 0; i <= 4; ++i) {
+            for (int j = 4; i <= j; --j) {
                 System.out.print("  ");
-                count = i*2;
+                count = i * 2;
             }
-            while (0 <= count){
+            while (0 <= count) {
                 System.out.print(" ");
                 System.out.print("*");
                 count--;
@@ -20,24 +20,24 @@ public class Figure {
 
     public void OutPutRhomb() {
         int count = 0;
-        for (int i=0; i<=4; ++i) {
-            for (int j=4; i<=j; --j){
+        for (int i = 0; i <= 4; ++i) {
+            for (int j = 4; i <= j; --j) {
                 System.out.print("  ");
-                count = i*2;
+                count = i * 2;
             }
-            while (0 <= count){
+            while (0 <= count) {
                 System.out.print(" ");
                 System.out.print("*");
                 count--;
             }
             System.out.println();
         }
-        for (int i=3; i>=0; --i) {
-            for (int j=4; i<=j; --j){
+        for (int i = 3; i >= 0; --i) {
+            for (int j = 4; i <= j; --j) {
                 System.out.print("  ");
-                count = i*2;
+                count = i * 2;
             }
-            while (0 <= count){
+            while (0 <= count) {
                 System.out.print(" ");
                 System.out.print("*");
                 count--;
--- src/main/java/Lesson4/Main.java
+++ src/main/java/Lesson4/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int arr[] = {1,7,31,4,-2,4,-1,5,18,99,32};
+        int arr[] = {1, 7, 31, 4, -2, 4, -1, 5, 18, 99, 32};
         System.out.println("Write 1 in terminal to output Triangle");
         System.out.println("Write 2 in terminal to output Rhomb");
         System.out.println("Write 3 in terminal to output sorted massive");
@@ -14,26 +14,26 @@ public class Main {
 
 
         Figure Illustration = new Figure();
-        switch (KeyInterface){
-            case (1):Illustration.OutPutTriangle();
-            break;
-            case (2): Illustration.OutPutRhomb();
-            break;
+        switch (KeyInterface) {
+            case (1):
+                Illustration.OutPutTriangle();
+                break;
+            case (2):
+                Illustration.OutPutRhomb();
+                break;
             case (3):
-                for (int num: arr)
+                for (int num : arr)
                     System.out.print(num + " ");
 
                 Sorting.bubbleSort(arr);
                 System.out.println();
 
-                for (int num: arr
+                for (int num : arr
                 ) {
                     System.out.print(num + " ");
 
                 }
                 break;
-         }
-
-
+        }
     }
 }
